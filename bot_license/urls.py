9c70551e547430935abcb0a5c715f6edfc9af7bd
Covenant Monday <covenantmonday863@gmail.com>
2025-10-29 23:55:44 +0100

    added more endpoints

--- bot_license/urls.py
+++ bot_license/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CreateLicenseView, CreateProductView, RevokeLicenseView, VerifyLIcenseView, LicenseDetailsView
+from .views import CreateLicenseView, CreateProductView, RevokeLicenseView, VerifyLIcenseView, LicenseDetailsView, AllLicensesView, AllProductsView
 
 urlpatterns = [
     path('product/create/', CreateProductView.as_view(), name='create-product'),
@@ -7,4 +7,6 @@ urlpatterns = [
     path('license/verify/', VerifyLIcenseView.as_view(), name='verify-license'),
     path('license/revoke/', RevokeLicenseView.as_view(), name='revoke-license'),
     path('license/<str:license_key>/', LicenseDetailsView.as_view(), name='license-details'),
+    path('licenses/', AllLicensesView.as_view(), name='all-licenses'),
+    path('products/', AllProductsView.as_view(), name='all-products'),
 ]
